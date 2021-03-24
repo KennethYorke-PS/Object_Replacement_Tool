39111e68f04e183e198364dd04b98dad590b60d4
Kenneth-Yorke-Pineapple-Studios <80270527+Kenneth-Yorke-Pineapple-Studios@users.noreply.github.com>
2021-03-24 15:28:57 +0000

    Base Object Replacement Tool Implemented.
    - Implemented Basic Object Replacement Tool.
    - Implemented Basic Scene Selector Tool.

---
+++ Assets/Object_Replacement_Tool/Scripts/Editor/ReplaceSelectedObject.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Some code has been referenced and adapted from https://unity3d.college/2017/09/07/replace-gameobjects-or-prefabs-with-another-prefab/
+
+public class ReplaceSelectedObject : EditorWindow
+{
+    private bool isUsingTiled;
+    [SerializeField] GameObject replacementObject = null;
+    [MenuItem("Pineapple Studios/Object Replacer")]
+    static void ReplaceSelectedWithObject()
+    {
+        GetWindow(typeof(ReplaceSelectedObject), true, "Object Replacement Tool");
+    }
+
+    private void OnGUI()
+    {
+        isUsingTiled = EditorGUILayout.Toggle("Using TILED", isUsingTiled);
+
+        replacementObject = EditorGUILayout.ObjectField("", replacementObject, typeof(GameObject), true) as GameObject;
+
+        EditorGUILayout.LabelField("Select at least one object to replace in scene, choose the prefab to replace the selection with then click Replace");
+
+        if (GUILayout.Button("Replace Selected With Object"))
+        {
+            var selection = Selection.gameObjects;
+
+            if (selection.Length == 0)
+            {
+                Debug.LogError("Please ensure you have selected at least one object in the scene and try again");
+            }
+
+            for (var i = selection.Length - 1; i >= 0; --i)
+            {
+                var selected = selection[i];
+                var objectToDelete = selection[i].gameObject;
+                GameObject newObject;
+
+                if (replacementObject != null)
+                {
+                    newObject = (GameObject)PrefabUtility.InstantiatePrefab(replacementObject);
+                }
+                else
+                {
+                    newObject = null;
+                    Debug.LogError("Please ensure you have assigned a replacement prefab object and try again.");
+                    break;
+                }
+
+                Undo.RegisterCreatedObjectUndo(newObject, "Replace Objects with Prefab");
+                newObject.transform.parent = selected.transform.parent;
+                newObject.transform.localPosition = selected.transform.localPosition;
+                newObject.transform.localRotation = selected.transform.localRotation;
+                newObject.transform.localScale = selected.transform.localScale;
+                newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
+                Undo.DestroyObjectImmediate(objectToDelete);
+            }
+        }
+
+        GUI.enabled = false;
+        EditorGUILayout.LabelField("Selected Object Count: " + Selection.objects.Length);
+    }
+}
---
+++ Assets/Object_Replacement_Tool/Scripts/Editor/SceneSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class SceneSelector : EditorWindow
+{
+    [MenuItem("Pineapple Studios/Scenes/Load Sample Scene")]
+    public static void LoadDesertScene()
+    {
+        EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+        EditorSceneManager.OpenScene("Assets/Scenes/SampleScene.unity");
+    }
+
+    [MenuItem("Pineapple Studios/Utility/Clear All PlayerPrefs")]
+    public static void ClearPlayerPrefs()
+    {
+        if (EditorUtility.DisplayDialogComplex("Clear All Player Prefs", "Are you sure you want to clear all Player Prefs?", "Yes please", "Not a chance", "Cancel") == 0)
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
